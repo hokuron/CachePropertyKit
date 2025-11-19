e418387c0920b871a9ebbd4ff011f23347251d36
Takuma Shimizu <takuma-shimizu@cybozu.co.jp>
2025-11-19 10:54:39 +0900

    Update Swift tool version and language mode to 6

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 6.0
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -19,17 +19,11 @@ let package = Package(
     ],
     targets: [
         .target(
-            name: "CachePropertyKit",
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
-            ]
+            name: "CachePropertyKit"
         ),
         .testTarget(
             name: "CachePropertyKitTests",
-            dependencies: ["CachePropertyKit"],
-            swiftSettings: [
-                .enableExperimentalFeature("StrictConcurrency")
-            ]
+            dependencies: ["CachePropertyKit"]
         ),
     ]
 )
